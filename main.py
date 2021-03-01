875dbe2932e503a60b2d4e4223409a47305d3b96
Michele Scarlato <michele.scarlato@gmail.com>
2021-03-02 00:13:29 +0900

    Fixed the fetching of array elements from the JSON

--- main.py
+++ main.py
@@ -3,30 +3,16 @@ from validate import validate
 
 
 print("Started Reading JSON report")
-with open("json/json-iterator.json", "r") as read_file:
+with open("json/javacpp.json", "r") as read_file:
     data = json.load(read_file)#dict
     license_list = []
     for i in data['payload']['fileMetadata']:
-        if not (i['licenses']) in license_list:
-            license_list.append(i['licenses'])
-license_list_cleaned = []
+        for x in (i['licenses']):
+              if not x in license_list:
+                  license_list.append(x)
+            # print(i['licenses'][1])
 
-#print ("Licenses found in this project are :")
-for i in license_list:
-    my_string = str(i).replace('[', '')
-    my_string = str(my_string).replace(']', '')
-    my_string = str(my_string).replace('\'', '')
-    license_list_cleaned.append(my_string)
-
-#print(license_list_cleaned)
 print("Finished reading JSON report")
 print("Licenses found are:")
-print(license_list_cleaned)
-
-validate(license_list_cleaned)
-
-
-
-# accessing-data-from-a-json-array-in-python
-#https://stackoverflow.com/questions/57799042/accessing-data-from-a-json-array-in-python/57799117
-#https://www.programiz.com/python-programming/json
+print(license_list)
+validate(license_list)
